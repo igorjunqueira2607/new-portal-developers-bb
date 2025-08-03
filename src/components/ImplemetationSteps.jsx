954fc657c033063ceb6f31d25a81c6f8f55e37f5
Igor <igormelojunqueira@gmail.com>
2025-08-03 20:26:20 -0300

    Banner da home finalizado

---
+++ src/components/ImplemetationSteps.jsx
@@ -0,0 +1,67 @@
+function ImplematationSteps () {
+  return(
+    <section class="flex flex-col absolute bottom-2/10 left-30 w-7/12 h-1/4 items-center">
+      <div class="flex w-9/10 justify-between items-center relative">
+        <hr class="text-amarelo-bb w-full absolute border-2"/>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">1</h1>
+        </div>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">2</h1>
+        </div>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">3</h1>
+        </div>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">4</h1>
+        </div>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">5</h1>
+        </div>
+      </div>
+      <div class="flex justify-between w-full mt-4">
+        <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
+          <div class="pt-4 pb-4 w-full flex flex-col items-center">
+            <img src="src/images/clipboard.png" class="w-14 h-14"/>
+            <h1 class="text-amarelo-bb mt-4">
+              Cadastre-se no Portal.
+            </h1>
+          </div>
+        </div>
+        <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
+          <div class="pt-4 pb-4 w-full flex flex-col items-center">
+            <img src="src/images/app-development.png" class="w-14 h-14"/>
+            <h1 class="text-amarelo-bb mt-4">
+              Crie sua aplicação.
+            </h1>
+          </div>
+        </div>
+        <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
+          <div class="pt-4 pb-4 w-full flex flex-col items-center">
+            <img src="src/images/hand.png" class="w-14 h-14"/>
+            <h1 class="text-amarelo-bb mt-4">
+              Escolha uma ou mais APIs.
+            </h1>
+          </div>
+        </div>
+        <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
+          <div class="pt-4 pb-4 w-full flex flex-col items-center">
+            <img src="src/images/icons8-teste-passado-100.png" class="w-14 h-14"/>
+            <h1 class="text-amarelo-bb mt-4">
+              Teste as APIs no Sandbox.
+            </h1>
+          </div>
+        </div>
+        <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
+          <div class="pt-4 pb-4 w-full flex flex-col items-center">
+            <img src="src/images/icons8-ordem-de-produção-96.png" class="w-14 h-14"/>
+            <h1 class="text-amarelo-bb mt-4">
+              Envie sua aplicação para a produção.
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default ImplematationSteps;
