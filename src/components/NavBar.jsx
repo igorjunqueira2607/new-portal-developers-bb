ea21d231470771ddc8897967dfc3bd25114f0974
Igor <igormelojunqueira@gmail.com>
2025-08-03 01:14:20 -0300

    Estrutura do NavBar pronta

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,31 @@
 function NavBar () {
   return(
-    <nav>
-
+    <nav class="h-16 bg-azul-bb p-3 gap-4 flex justify-between fixed w-screen border-b-4 border-amarelo-bb">
+      <div class="flex h-fit w-fit gap-4">
+        <button class="max-h-full cursor-pointer"><img class="w-7" src="src/images/icons8-cardápio.svg"/></button>
+        <img class="h-9 left-14" src="src/images/BancodoBrasil.Logomarca.Vers╞oPrincipal.Amarelo.RGB.svg"/>
+      </div>
+      <div class="h-full flex gap-5 ml-3 grow justify-items-start">
+        <button class="h-full flex w-fit items-center cursor-pointer">
+          <h1 class="text-branco-navbar content-center flex items-center">BaaS</h1>
+          <img src="src/images/arrow-down-svgrepo-com.svg" class="h-5 ml-2"/>
+        </button>
+        <button class="h-full flex w-fit items-center cursor-pointer">
+          <h1 class="text-branco-navbar content-center flex items-center">Para desenvolvedores</h1>
+          <img src="src/images/arrow-down-svgrepo-com.svg" class="h-5 ml-2"/>
+        </button>
+        <button class="h-full flex w-fit items-center cursor-pointer">
+          <h1 class="text-branco-navbar content-center flex items-center">Parcerias</h1>
+          <img src="src/images/arrow-down-svgrepo-com.svg" class="h-5 ml-2"/>
+        </button>
+        <button class="h-full flex w-fit items-center cursor-pointer">
+          <h1 class="text-branco-navbar content-center flex items-center">Suporte</h1>
+          <img src="src/images/arrow-down-svgrepo-com.svg" class="h-5 ml-2"/>
+        </button>
+      </div>
+      <button class="bg-amarelo-bb pl-14 pr-14 ">
+        <h1 class="text-azul-bb">Entrar</h1>
+      </button>
     </nav>
   )
 }
