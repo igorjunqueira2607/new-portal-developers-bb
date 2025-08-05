e38d51aa9d48cc49768f98aaf54c76fca3759b56
Igor <igormelojunqueira@gmail.com>
2025-08-04 22:47:22 -0300

    alterando o caminho das imagens3

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -4,7 +4,7 @@ import ImplematationSteps from "./ImplemetationSteps";
 function BannerHome () {
   return (
     <div class="w-full h-screen overflow-hidden mt-16 relative flex">
-      <img src="/src/images/Banner_Home-1.webp" class="w-max-full h-full scale-x-101 z-0"/>
+      <img src="public/static/images/Banner_Home-1.webp" class="w-max-full h-full scale-x-101 z-0"/>
       <div class="absolute top-1/6 w-5/12 text-justify left-1/7">
         <h1 class ="text-amarelo-bb text-8xl font-bold mb-5">Portal Developers BB</h1>
         <h2 class="text-amarelo-bb text-2xl font-normal">Bem-vindo(a) ao Portal Developers do BB. Aqui você irá encontrar tudo que precisa para conectar seu negócio com as APIs do Banco do Brasil de uma maneira simples e fácil.</h2>
