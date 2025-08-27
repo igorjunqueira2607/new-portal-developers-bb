f3c66dd7112e694db47899f13abb231354c7f540
Igor Melo Junqueira <igormelojunqueira@gmail.com>
2025-08-27 09:51:51 -0300

    Mudando cor 2

--- src/components/ImplematationSection.jsx
+++ src/components/ImplematationSection.jsx
@@ -2,8 +2,8 @@ import ImplematationSteps from "./ImplemetationSteps";
 
 function ImplematationSection () {
   return (
-    <section class="w-full h-fit overflow-hidden items-center relative flex flex-col bg-amarelo-bb pt-30 pb-30">
-      <h1 class="text-azul-bb text-6xl font-bold mb-50">Crie sua aplicação de maneira simples e fácil!</h1>
+    <section class="w-full h-fit overflow-hidden items-center relative flex flex-col bg-azul-bb pt-30 pb-30">
+      <h1 class="text-amarelo-bb text-6xl font-bold mb-50">Crie sua aplicação de maneira simples e fácil!</h1>
       <ImplematationSteps />
     </section>
   )
