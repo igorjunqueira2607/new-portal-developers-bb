72d898453e261dc022d0a8b6e0fd1676b987c98c
Igor Melo Junqueira <igormelojunqueira@gmail.com>
2025-08-27 09:50:07 -0300

    Mudando cor

--- src/components/ImplemetationSteps.jsx
+++ src/components/ImplemetationSteps.jsx
@@ -2,28 +2,28 @@ function ImplematationSteps () {
   return(
     <section class="flex flex-col w-4/5 h-1/4 items-center">
       <div class="flex w-full justify-between items-center relative">
-        <hr class="text-azul-bb w-full absolute border-2"/>
-        <div class="bg-amarelo-bb rounded-full border-3 border-azul-bb flex relative w-16 h-16 justify-center items-center">
-          <h1 class="text-2xl text-azul-bb font-semibold">1</h1>
+        <hr class="text-amarelo-bb w-full absolute border-2"/>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">1</h1>
         </div>
-        <div class="bg-amarelo-bb rounded-full border-3 border-azul-bb flex relative w-16 h-16 justify-center items-center">
-          <h1 class="text-2xl text-azul-bb font-semibold">2</h1>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">2</h1>
         </div>
-        <div class="bg-amarelo-bb rounded-full border-3 border-azul-bb flex relative w-16 h-16 justify-center items-center">
-          <h1 class="text-2xl text-azul-bb font-semibold">3</h1>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">3</h1>
         </div>
-        <div class="bg-amarelo-bb rounded-full border-3 border-azul-bb flex relative w-16 h-16 justify-center items-center">
-          <h1 class="text-2xl text-azul-bb font-semibold">4</h1>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">4</h1>
         </div>
-        <div class="bg-amarelo-bb rounded-full border-3 border-azul-bb flex relative w-16 h-16 justify-center items-center">
-          <h1 class="text-2xl text-azul-bb font-semibold">5</h1>
+        <div class="bg-azul-bb rounded-full border-3 border-amarelo-bb flex relative w-16 h-16 justify-center items-center">
+          <h1 class="text-2xl text-amarelo-bb font-semibold">5</h1>
         </div>
       </div>
       <div class="flex justify-between w-8/7 mt-4">
         <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
           <div class="pt-4 pb-4 w-full flex flex-col items-center">
             <img src="/assets/clipboard.png" class="w-14 h-14"/>
-            <h1 class="text-azul-bb mt-4 text-xl font-medium">
+            <h1 class="text-amarelo-bb mt-4 text-xl font-medium">
               Cadastre-se no Portal.
             </h1>
           </div>
@@ -31,7 +31,7 @@ function ImplematationSteps () {
         <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
           <div class="pt-4 pb-4 w-full flex flex-col items-center">
             <img src="assets/app-development.png" class="ml-2 w-14 h-14"/>
-            <h1 class="text-azul-bb mt-4 text-xl font-medium">
+            <h1 class="text-amarelo-bb mt-4 text-xl font-medium">
               Crie sua aplicação.
             </h1>
           </div>
@@ -39,7 +39,7 @@ function ImplematationSteps () {
         <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
           <div class="pt-4 pb-4 w-full flex flex-col items-center">
             <img src="/assets/hand.png" class="ml-2 w-14 h-14"/>
-            <h1 class="text-azul-bb mt-4 text-xl font-medium">
+            <h1 class="text-amarelo-bb mt-4 text-xl font-medium">
               Escolha uma ou mais APIs.
             </h1>
           </div>
@@ -47,7 +47,7 @@ function ImplematationSteps () {
         <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
           <div class="pt-4 pb-4 w-full flex flex-col items-center">
             <img src="/assets/icons8-teste-passado-100.png" class="ml-2 w-14 h-14"/>
-            <h1 class="text-azul-bb mt-4 text-xl font-medium">
+            <h1 class="text-amarelo-bb mt-4 text-xl font-medium">
               Teste as APIs no Sandbox.
             </h1>
           </div>
@@ -55,7 +55,7 @@ function ImplematationSteps () {
         <div class="text-center w-1/6 h-full justify-between flex flex-col items-center">
           <div class="pt-4 pb-4 w-full flex flex-col items-center">
             <img src="/assets/icons8-ordem-de-produção-96.png" class="ml-2 w-14 h-14"/>
-            <h1 class="text-azul-bb mt-4 text-xl font-medium">
+            <h1 class="text-amarelo-bb mt-4 text-xl font-medium">
               Envie sua aplicação para a produção.
             </h1>
           </div>
